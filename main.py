7dc1a56c3240dca77b1b44a06939c27ce5fff2b3
tasleem-m <trmuhammad2017@gmail.com>
2026-02-07 10:31:48 -0600

    api ingestion

--- main.py
+++ main.py
@@ -13,12 +13,15 @@ OBJECT_PATH = os.getenv("RAW_OBJECT_PATH")
 
 
 def main():
+## Census API
     logger.info("Starting Census data pipeline")
     api_key = os.getenv("CENSUS_API_KEY")
 
     census_data = fetch_census_data(api_key)
     upload_to_s3(census_data, "api", "census_acs5_2022.csv")
     logger.info("Pipeline complete")
+
+    d.convert_to_duckdb(SCHEMA, "census_api", census_data, logger)
 
 # NPPES
 
